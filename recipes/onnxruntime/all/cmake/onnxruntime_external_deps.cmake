bb0f44ff896e2a1d10ac0be93a021f9aadf8e3e3
Gerry Gralton <gerry.gralton@openworksengineering.com>
2026-05-20 15:04:04 +0100

    Add tensorrt external dep

--- recipes/onnxruntime/all/cmake/onnxruntime_external_deps.cmake
+++ recipes/onnxruntime/all/cmake/onnxruntime_external_deps.cmake
@@ -110,6 +110,13 @@ set(CUDNN_FRONTEND_BUILD_PYTHON_BINDINGS OFF CACHE BOOL "" FORCE)
 set(CUDNN_PATH ${onnxruntime_CUDNN_HOME})
 onnxruntime_fetchcontent_makeavailable(cudnn_frontend)
 
+onnxruntime_fetchcontent_declare(
+ onnx_tensorrt
+ URL https://github.com/onnx/onnx-tensorrt/archive/d5dce67db7c2e64b07e055571f5ec06f7f254de2.zip
+ URL_HASH SHA1=01114d3b67650857281fa50faa2e412130a63b69
+ EXCLUDE_FROM_ALL
+)
+
 set(onnxruntime_LINK_DIRS)
 if (onnxruntime_USE_CUDA)
   find_package(CUDAToolkit REQUIRED)
